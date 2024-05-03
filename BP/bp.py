ff591b325e7960a799b9901533d46780a27d3eae
7zZFHpYyu9 <jianguoh003@gmail.com>
2024-05-03 18:04:52 +0800

    BP

---
+++ BP/bp.py
@@ -0,0 +1,15 @@
+from numpy import array, exp, random, dot
+
+x = array()
+y = array().T
+random.seed(1)
+weights = 2 * random.random((3, 1)) - 1
+
+for it in range(10000):
+    output = 1/(1+exp(-dot(x, weights)))
+    error = y - output
+    delta = error * output * (1-output)
+    weights += dot(x.T, delta)
+
+print(weights)
+print(1/(1+exp(-dot([[1,0,0]], weights))))
